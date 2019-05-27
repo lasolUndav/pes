25b016383a1d35c0487c4d4131609a0594657a9a
janeko23 <munozjeanette23@gmail.com>
2019-05-26 21:46:22 -0300

    Cambios realizados

--- web/src/app/pes-admin/providers/providers.component.ts
+++ web/src/app/pes-admin/providers/providers.component.ts
@@ -50,11 +50,12 @@ export class ProvidersComponent implements OnInit {
 
   copyText(val: Provider) {
     const selBox = document.createElement('textarea')
+    const conCuit = val.cuilCuit + '\n' + val.numAccount
     selBox.style.position = 'fixed'
     selBox.style.left = '0'
     selBox.style.top = '0'
     selBox.style.opacity = '0'
-    selBox.value = val.cuilCuit + '\n ' + val.numAccount
+    selBox.value = val.cuilCuit.length === 0 ? val.numAccount : conCuit
     document.body.appendChild(selBox)
     selBox.focus()
     selBox.select()
