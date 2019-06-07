faf830eac944c2e6caf194aacc5bb265716d73ba
fedexdiaz <federicodiaz545@gmail.com>
2019-06-07 12:07:53 -0300

    Varios refactors en la autenticacion de pes

--- web/src/app/layout/header/header.component.ts
+++ web/src/app/layout/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core'
 
+import { AuthService } from 'src/app/auth/auth.service'
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  constructor(public authService: AuthService) {}
 
   ngOnInit() {}
 }
