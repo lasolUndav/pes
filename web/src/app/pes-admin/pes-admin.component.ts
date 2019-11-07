67145e95be3762dd09fd6cbc21c9f51efb5d34ab
federico <fede@intrigma.com>
2019-11-07 11:01:09 -0300

    Se agregar el site-map de navegacion

--- web/src/app/pes-admin/pes-admin.component.ts
+++ web/src/app/pes-admin/pes-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+
 import { AuthService } from '../auth/auth.service'
 import { Router } from '@angular/router'
 
@@ -8,7 +9,14 @@ import { Router } from '@angular/router'
   styleUrls: ['./pes-admin.component.css'],
 })
 export class PesAdminComponent implements OnInit {
-  constructor(public authService: AuthService, public router: Router) {}
+  siteMapLabel: string
+  constructor(public authService: AuthService, public router: Router) {
+    this.siteMapLabel = 'Administración/proveedores'
+  }
+
+  changeSiteMapLabel(page) {
+    this.siteMapLabel = `Administración/${page}`
+  }
 
   ngOnInit() {}
   logout() {
