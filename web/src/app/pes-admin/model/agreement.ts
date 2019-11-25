e06ec9ff61d4d77908cbee725b7dcf5800436b34
LisandroEmmanuel <lisandroemmanuelgil@gmail.com>
2019-11-25 17:38:51 -0300

    Se realizaron los cambios pedidos

--- web/src/app/pes-admin/model/agreement.ts
+++ web/src/app/pes-admin/model/agreement.ts
@@ -54,18 +54,4 @@ export class Agreement {
 
     return dto
   }
-
-  /*public separateCategorys(categorias: Array<AgreementTransactionCategory>) {
-    let vuelta: number
-    vuelta = 0
-    for (const categoria of categorias) {
-      if (vuelta == 0) {
-        this.categorias += categoria.name
-      }
-      else {
-        this.categorias += ',' + categoria.name
-      }
-    }
-  }
-  */
 }
